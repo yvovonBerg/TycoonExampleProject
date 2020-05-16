bf69b9f58eb74c9188f2bcfa383f196eef8fab38
yvo von berg <yvo_von_berg@hotmail.com>
2020-05-17 00:35:35 +0100

    added example project

---
+++ Source/TycoonExample/TycoonExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TycoonExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TYCOONEXAMPLE_API ATycoonExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
